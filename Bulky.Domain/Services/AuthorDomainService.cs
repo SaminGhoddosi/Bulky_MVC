ac38c3f10ac78c0ab16fbf9265a043fe7db7be27
Samin Ghoddosi <saminghoddosi44@gmail.com>
2026-06-17 13:46:36 -0300

    Add DI in Domain

--- Bulky.Domain/Services/AuthorDomainService.cs
+++ Bulky.Domain/Services/AuthorDomainService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Bulky.Domain.Entities;
+using Bulky.Domain.Interfaces.IServices;
+using Bulky.Domain.Interfaces.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace Bulky.Domain.Services
 {
-    public class AuthorDomainService
+    public class AuthorDomainService : DomainService<Author>, IAuthorDomainService
     {
+        private readonly IAuthorRepository _repository; 
+        public AuthorDomainService(IAuthorRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
     }
 }
