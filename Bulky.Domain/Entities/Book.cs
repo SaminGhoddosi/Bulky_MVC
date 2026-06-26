fe203238ce6e6f229ffee3a70f0da236384f8f33
Samin Ghoddosi <saminghoddosi44@gmail.com>
2026-06-26 15:32:56 -0300

    EF Core type configs

--- Bulky.Domain/Entities/Book.cs
+++ Bulky.Domain/Entities/Book.cs
@@ -16,7 +16,7 @@ namespace Bulky.Domain.Entities
         public string Name { get; set; }
         public string? Description { get; set; }
         public string ISBN { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public string? ImageUrl { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
---
+++ Bulky.Infra/TypeConfigurations/BookTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Infra.TypeConfigurations
+{
+    public class BookTypeConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.ISBN).HasColumnType("nvarchar(255)");
+            builder.Property(x => x.Price).HasColumnType("decimal(10,2)");
+            builder.Property(x => x.Description).HasColumnType("nvarchar(255)");
+            builder.Property(x => x.CategoryId).HasColumnType("int");
+            builder.Property(x => x.Name).HasColumnType("nvarchar(255)");
+            builder.Property(x => x.ImageUrl).HasColumnType("nvarchar(255)");
+        }
+    }
+}
