73536b27e932897297f58c552fad03df06adb443
Samin Ghoddosi <saminghoddosi44@gmail.com>
2026-06-30 09:52:55 -0300

    Add app models

---
+++ Bulky.Application/AppModel/CategoryAppModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Application.AppModel
+{
+    public class CategoryAppModel : BaseAppModel
+    {
+        public string Name { get; set; }
+    }
+}
